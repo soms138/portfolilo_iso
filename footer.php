580d0e5d8629f26e798095f4d394f1ef23d95152
somin <thals0313@naver.com>
2023-09-13 14:16:39 +0900

    중간점검

---
+++ footer.php
@@ -0,0 +1,20 @@
+<footer>
+    <div class="fnb_left">
+        <div class="fnb_title">
+            <span>LEESOMIN</span>
+            <span>VISUAL DESIGN</span>
+            <span>2023 PORTFOLIO</span>
+        </div>
+        <div class="fnb_contents">
+            <span>BEYOND</span>
+            <span>THE HORIZON</span>
+            <span>WEBPUBLISHER</span>
+        </div>
+    </div>
+    <div class="fnb_right">
+        <a href="#" class="intro active">INTRO</a>
+        <a href="#" class="about">ABOUT</a>
+        <a href="#" class="web">WEB</a>
+        <a href="#" class="graphic">GRAPHIC</a>
+    </div>
+</footer>
--- script/index.js
+++ script/index.js
@@ -22,7 +22,46 @@ const news_slide = new Swiper('#news_slide',{
     direction: 'horizontal',
     speed:1000,
     navigation: {
-        nextEl: '#news_slide .swiper-button-next',
-        prevEl: '#news_slide .swiper-button-prev'
+        nextEl: '.slide_btn .swiper-button-next',
+        prevEl: '.slide_btn .swiper-button-prev'
     }
+})
+const tab_title = document.querySelectorAll('.tab_title a')
+const tab_contents = document.querySelectorAll('.tab_contents > div')
+const tab_detail = document.querySelectorAll('.tab_detail')
+const sns_contents = document.querySelectorAll('.sns_contents')
+const bnr = document.querySelectorAll('#bnr_slide .swiper-wrapper .swiper-slide')
+const news = document.querySelectorAll('#news_slide .swiper-wrapper .swiper-slide')
+console.log(tab_contents,tab_title,tab_detail,sns_contents,bnr,news)
+for(let i of tab_contents){i.style.display = 'none'}
+for(let i of tab_detail){i.style.display = 'none'}
+tab_contents[0].style.display = 'block'
+tab_detail[0].style.display = 'block'
+tab_title.forEach(function(t,i){
+    t.addEventListener('click', function(e){
+        e.preventDefault()
+        for(let i of tab_title){i.classList.remove('active')}
+        t.classList.add('active')
+        for(let i of tab_contents){i.style.display = 'none'}
+        tab_contents[i].style.display = 'block'
+        tab_detail[i].style.display = 'block'
+    })
+})
+sns_contents.forEach(function(t){
+    t.addEventListener('click',function(e){
+        e.preventDefault()
+        t.classList.toggle('show')
+    })
+})
+bnr.forEach(function(t){
+    t.addEventListener('click',function(e){
+        e.preventDefault()
+        t.classList.toggle('show')
+    })
+})
+news.forEach(function(t){
+    t.addEventListener('click',function(e){
+        e.preventDefault()
+        t.classList.toggle('show')
+    })
 })
